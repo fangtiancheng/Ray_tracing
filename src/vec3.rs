adbec6aabe5d163e0915f2065f78d071ac81ae51
fangtiancheng <1773701277@qq.com>
2020-07-27 15:41:38 +0800

    many thread

--- src/vec3.rs
+++ src/vec3.rs
@@ -13,12 +13,16 @@ impl Vec3 {
         Self { x, y, z }
     }
 
-    pub fn ones() -> Self {
-        Self::new(1.0, 1.0, 1.0)
+    pub const fn ones() -> Self {
+        return Self{
+            x:1.0,y:1.0,z:1.0
+        };
     }
 
-    pub fn zero() -> Self {
-        Self::new(0.0, 0.0, 0.0)
+    pub const fn zero() -> Self {
+        return Self{
+            x:0.0, y:0.0, z:0.0
+        };
     }
     pub fn random()->Self {
         return Self{
